90e438080fb774e33ac86d713738f4a1a6554da8
Jina Suh <jasmart@gmail.com>
2015-05-02 13:47:15 -0700

    Add the gray in the legend

--- scripts/legend.js
+++ scripts/legend.js
@@ -17,9 +17,11 @@
     legendInstance.update = function () {
         if (data && !initialized) {
             initialized = true;
-            var logFoiRange = [1, 2, 3, 4, 5, 6, 7];
-            var foiThreshold = d3.scale.threshold().domain(logFoiRange).range(data.colorScheme);
-            var legend = d3.svg.legend().units("log(FOI)").cellWidth(80).cellHeight(25).inputScale(foiThreshold).cellStepping(100);
+            var logFoiRange = [0, 1, 2, 3, 4, 5, 6, 7];
+            var withZero = data.colorScheme.slice();
+            withZero.unshift("#eee");
+            var foiThreshold = d3.scale.threshold().domain(logFoiRange).range(withZero);
+            var legend = d3.svg.legend().units("log(1+FOI)").cellWidth(80).cellHeight(25).inputScale(foiThreshold).cellStepping(100);
 
             d3.select(containerId).append("svg")
                 .attr("width", width + margin.left + margin.right)

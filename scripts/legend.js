ddeb0fde1dfa400dd813b3e654d41fd86ed2cf95
Jina Suh <jasmart@gmail.com>
2015-05-01 23:05:44 -0700

    Fix size to 1440x900

--- scripts/legend.js
+++ scripts/legend.js
@@ -1,7 +1,12 @@
 ﻿viz.legend = function (containerId) {
 
+    containerId = "#" + containerId;
     var legendInstance = {};
     var initialized = false;
+    var margin = { top: 0, right: 10, bottom: 0, left: 10 };
+
+    var width = $(containerId).width() - margin.left - margin.right;
+    var height = $(containerId).height() - margin.top - margin.bottom;
 
     legendInstance.data = function (_) {
         if (!arguments.length) return data;
@@ -16,7 +21,10 @@
             var foiThreshold = d3.scale.threshold().domain(logFoiRange).range(data.colorScheme);
             var legend = d3.svg.legend().units("log(FOI)").cellWidth(80).cellHeight(25).inputScale(foiThreshold).cellStepping(100);
 
-            d3.select("#" + containerId).append("svg").append("g").attr("transform", "translate(1,70)").attr("class", "legend").call(legend);
+            d3.select(containerId).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g").attr("transform", "translate(1,70)").attr("class", "legend").call(legend);
         }
         return legendInstance;
     };
